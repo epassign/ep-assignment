fedcb74eeb1c3a6928a891623996134cb5600305
Adrian Praja <adrian@databank.ro>
2021-07-27 22:30:58 +0300

    trigger deploy

--- lambda/website/index.js
+++ lambda/website/index.js
@@ -3,7 +3,7 @@ exports.handler = function( event, context, cb ) {
 	cb(null, {
 		statusCode: 404,
 		headers: {'contentType': 'text/html',},
-		body: '[' + event._PATH + '] Not Found!' + JSON.stringify( event, null, "\t"),
+		body: '[' + event._PATH + '] Not Found! <br>' + JSON.stringify( event, null, "\t"),
 	})
 
 }
