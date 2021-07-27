a975cf8cea65584dbbd55beffb3d68f905355200
Adrian Praja <adrian@databank.ro>
2021-07-27 23:51:16 +0300

    try include cdn script

--- lambda/website/pages/index.js
+++ lambda/website/pages/index.js
@@ -8,12 +8,10 @@ module.exports = function( event, cb ) {
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
 		<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+		<script src="https://${process.env.cdn_master}/ep-assignment.min.js"></script>
 	</head>
 	<body>
-		<h1>index</h1>
 
-		<a href="/login">Login</a>
-		<a href="/signup">Signup</a>
 	</body>
 </html>
 
