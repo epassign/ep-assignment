1bacc09a42aa2a6d385c5dc888afe8ee510822aa
Adrian Praja <adrian@databank.ro>
2021-07-27 23:12:07 +0300

    bootstrap+react from CDN

--- lambda/website/pages/404.js
+++ lambda/website/pages/404.js
@@ -2,15 +2,23 @@
 module.exports = function( event, cb ) {
 	cb({
 		statusCode: 404,
-		body: `
-			<div style="text-align: center;margin-top: 100px;">
-				<h1>404 - NOT FOUND</h1>
+		body: `<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Not Found</title>
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+			</head>
+			<body>
+				<div style="text-align: center;margin-top: 100px;">
+					<h1>404 - NOT FOUND</h1>
 
-				<pre style="width: 500px;height: 400px;font-size: 12px;text-align: left;margin: 0 auto;border: 1px solid #ccc;padding: 10px;overflow: auto;">` 
-					+ JSON.stringify( event, null, "\t") 
-				+ `
-				</pre>
-			</div>
+					<pre style="width: 500px;height: 400px;font-size: 12px;text-align: left;margin: 0 auto;border: 1px solid #ccc;padding: 10px;overflow: auto;">` 
+						+ JSON.stringify( event, null, "\t") 
+					+ `
+					</pre>
+				</div>
+			</body>
+		</html>
 		`,
 	})
 }
--- lambda/website/pages/index.js
+++ lambda/website/pages/index.js
@@ -5,6 +5,9 @@ module.exports = function( event, cb ) {
 <html>
 	<head>
 		<title>Index</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+		<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
 	</head>
 	<body>
 		<h1>index</h1>
