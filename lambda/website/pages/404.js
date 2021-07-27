78f4b5754756dbc40ce39dde31ab3a9dc9fc11a7
Adrian Praja <adrian@databank.ro>
2021-07-27 22:52:11 +0300

    404 page

--- lambda/website/pages/404.js
+++ lambda/website/pages/404.js
@@ -3,11 +3,12 @@ module.exports = function( event, cb ) {
 	cb({
 		statusCode: 404,
 		body: `
-			<div style="text-align: center;">
+			<div style="text-align: center;margin-top: 100px;">
 				<h1>404 - NOT FOUND</h1>
 
-				<pre style="width: 500px;height: 400px;font-size: 12px;text-align: left;overflow: auto;">
-					` + JSON.stringify( event, null, "\t") + `
+				<pre style="width: 500px;height: 400px;font-size: 12px;text-align: left;margin: 0 auto;border: 1px solid #ccc;padding: 10px;overflow: auto;">` 
+					+ JSON.stringify( event, null, "\t") 
+				+ `
 				</pre>
 			</div>
 		`,
