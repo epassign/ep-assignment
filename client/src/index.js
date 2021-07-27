6105666cc05fc4e72abaaea96c906cce9eb2d2a5
Adrian Praja <adrian@databank.ro>
2021-07-28 01:14:51 +0300

    navbar

--- client/src/index.js
+++ client/src/index.js
@@ -24,21 +24,34 @@ class App extends React.Component {
 		return (
 
 			<Router>
-				<div>
-					<nav>
-						<ul>
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/login">Login</Link>
-							</li>
-							<li>
-								<Link to="/signup">Signup</Link>
-							</li>
-						</ul>
-					</nav>
 
+				<nav class="navbar navbar-expand-lg bg-dark">
+					<div class="container-fluid"> 
+						<a class="navbar-brand" href="#">
+							
+						</a> 
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> 
+							<span class="navbar-toggler-icon"></span> 
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav ml-auto"> 
+								<Link class="nav-link active" to="/">HOME</Link> 
+
+								<Link class="nav-link" to="/login">Login</Link>
+								<Link class="nav-link" to="/signup">Signup</Link>
+
+							</div>
+						</div>
+					</div>
+
+					<form class="container-fluid justify-content-end">
+						<Link class="btn btn-sm btn-outline-secondary" to="/account"> <img src="https://i.imgur.com/C4egmYM.jpg" class="rounded-circle" width="30" /> Account</Link>
+					</form>
+				</nav>
+
+
+
+				<div class="container">
 					<Switch>
 						<Route exact path="/">
 							<Home />
