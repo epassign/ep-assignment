705edc76e27b429c705a386c8b4410033d9eb4db
Adrian Praja <adrian@databank.ro>
2021-07-27 18:09:33 +0300

    test Github Actions integration

--- lambda/website/index.js
+++ lambda/website/index.js
@@ -1,7 +1,9 @@
 exports.handler = function( event, context, cb ) {
+
 	cb(null, {
 		statusCode: 404,
 		headers: {'contentType': 'text/html',},
 		body: '[' + event._PATH + '] Not Found!' + JSON.stringify( event, null, "\t"),
 	})
+
 }
