ea2d24b6a66751aa265cd5e8b0283e804401ce2d
Adrian Praja <adrian@databank.ro>
2021-07-28 13:45:50 +0300

    GET /v1/history

--- lambda/website/v1/history/GET.js
+++ lambda/website/v1/history/GET.js
@@ -4,7 +4,16 @@ module.exports = function(event, cb) {
 
 
 
-	var history = [];
+	//var history = [];
+
+	var usd_history = {}
+	for (let start = new Date().getTime() - (1000 * 60*60 ); start <= new Date().getTime(); start+=1000*30 ) {
+		usd_history[ new Date(new Date(start).toISOString().slice(0,16) + ':00.00Z').getTime() ] = 0
+	}
+	var eur_history = {}
+	for (let start = new Date().getTime() - (1000 * 60*60 ); start <= new Date().getTime(); start+=1000*30 ) {
+		eur_history[ new Date(new Date(start).toISOString().slice(0,16) + ':00.00Z').getTime() ] = 0
+	}
 
 	async.waterfall([
 
@@ -17,7 +26,11 @@ module.exports = function(event, cb) {
 				.query()
 				.then(( data ) => {
 
-					history = data
+					//history = data
+					data.map( ( d ) => {
+						usd_history[ new Date( d.minute + ':00.00Z').getTime() ] = d.usd
+						eur_history[ new Date( d.minute + ':00.00Z').getTime() ] = d.eur
+					})
 
 					cb()
 				})
@@ -37,7 +50,10 @@ module.exports = function(event, cb) {
 			statusCode: 200,
 			body: JSON.stringify({
 				success: true,
-				history,
+				history: {
+					usd: usd_history,
+					eur: eur_history,
+				},
 			}, null, "\t")
 		})
 	})
