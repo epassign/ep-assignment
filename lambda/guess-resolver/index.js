d9e12ed783f5c3dc64f183d15ad27e96d25e707c
Adrian Praja <adrian@databank.ro>
2021-07-28 18:14:37 +0300

    update

--- lambda/guess-resolver/index.js
+++ lambda/guess-resolver/index.js
@@ -12,10 +12,44 @@ exports.handler = function( event, context ) {
 
 	var default_sleep = 60; // 1 minute
 
+	var current_usd_rate;
 
 	async.waterfall([
 
 
+		// get the most recent BTC rate
+		( cb ) => {
+			DynamoDB
+				.table('btc_history')
+				.where('all').eq( 1 )
+				.desc()
+				.limit(1)
+				.query()
+				.then(( data ) => {
+					if (!data.length)
+						return cb({
+							_POST: event._POST,
+							sleep: 60*60*24,
+							end: true, // exit if failed
+						})
+
+					console.log( data[0] )
+					current_usd_rate = data[0].usd
+
+					cb()
+				})
+				.catch((err) => {
+					return cb({
+						_POST: event._POST,
+						sleep: 60*60*24,
+						end: true, // exit if failed
+					})
+				})
+		},
+
+
+
+		
 	], function(err) {
 		if (err) {
 			if (err.end === true )
