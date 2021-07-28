9d941876a1f0a398f30d5307ea1c815e65c82a59
Adrian Praja <adrian@databank.ro>
2021-07-28 08:55:10 +0300

    signup call rest api

--- client/src/signup.js
+++ client/src/signup.js
@@ -1,7 +1,32 @@
 
+import { api_put } from './common';
+import Cookies from 'js-cookie';
+
 class Signup extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			name: '',
+			username: '',
+			password: '',
+		}
+	}
+
+	submit() {
+		this.setState({ err: undefined })
+		api_put('/v1/account', {
+			name: this.state.name,
+			username: this.state.username,
+			password: this.state.password,
+		}, ( err, data ) => {
+			if (err)
+				return this.setState({ err: err.errorMessage || err.errorCode })
+
+			// save cookie and redirect
+			Cookies.set('asid', data.session_id, { path: '/', expires: 30 });
+			location.href = "/account"
+
+		})
 	}
 
 	render() {
@@ -19,21 +44,27 @@ class Signup extends React.Component {
 
 									<div class="form-outline mb-3">
 										<label class="form-label" for="name">Name</label>
-										<input type="text" id="name" class="form-control form-control-md" />
+										<input type="text" id="name" class="form-control form-control-md" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value }) } />
 									</div>
 
 									<div class="form-outline mb-3">
 										<label class="form-label" for="username">Username</label>
-										<input type="text" id="username" class="form-control form-control-md" />
+										<input type="text" id="username" class="form-control form-control-md" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value }) } />
 									</div>
 
 									<div class="form-outline mb-3">
 										<label class="form-label" for="password">Password</label>
-										<input type="password" id="password" class="form-control form-control-md" />
+										<input type="password" id="password" class="form-control form-control-md" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value }) } />
+									</div>
+
+									<div class="d-grid mx-auto" style={{height: '32px', }}>
+										<div class="err text-center" style={{fontSize: '10px', color: 'red', lineHeight: '32px', }}>
+										{ this.state.err }
+										</div>
 									</div>
 
 									<div class="d-grid gap-2 mx-auto">
-										<button class="btn btn-primary btn-md" type="submit">Create Account</button>
+										<button class="btn btn-primary btn-md" type="submit" onClick={this.submit.bind(this)}>Create Account</button>
 									</div>
 									
 
