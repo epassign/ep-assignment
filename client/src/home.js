a548774155e981c01eb6ae8d4f383a6b64ac01cd
Adrian Praja <adrian@databank.ro>
2021-07-29 09:53:44 +0300

    Home page - use Chart component

--- client/src/home.js
+++ client/src/home.js
@@ -1,4 +1,6 @@
 
+import BTCChart from './btc_chart_with_player';
+
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
@@ -6,7 +8,35 @@ class Home extends React.Component {
 
 	render() {
 		return (
-			<div>home</div>
+			<div class="container">
+				<div class="row mt-5">
+					<div class="col">
+						<BTCChart auth={this.props.auth} guess={this.props.guess} />
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col">
+						<div class="card">
+							<div class="card-header">
+								Hall of Fame
+							</div>
+							<div class="card-body">
+
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<div class="card-header">
+								Recents
+							</div>
+							<div class="card-body">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
