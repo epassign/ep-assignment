7de3eab67c964a1f0dd36940fafb10d558c84bbd
Adrian Praja <adrian@databank.ro>
2021-07-29 09:16:07 +0300

    signup - submit using ENTER key, auto lowercase username

--- client/src/signup.js
+++ client/src/signup.js
@@ -12,7 +12,10 @@ class Signup extends React.Component {
 		}
 	}
 
-	submit() {
+	submit(e) {
+
+		e.preventDefault()
+
 		this.setState({ err: undefined })
 		api_put('/v1/account', {
 			name: this.state.name,
@@ -38,35 +41,37 @@ class Signup extends React.Component {
 							<div class="card shadow-2-strong" style={{ borderRadius: '1rem', backgroundColor: '#efefef', }}>
 								<div class="card-body p-5">
 
-									<h3 class="mb-4 text-center">Signup</h3>
+									<form onSubmit={this.submit.bind(this)}>
 
-									<h6 class="mb-4 text-center">Already have an account ? <a href="/login">Login</a></h6>
+										<h3 class="mb-4 text-center">Signup</h3>
 
-									<div class="form-outline mb-3">
-										<label class="form-label" for="name">Name</label>
-										<input type="text" id="name" class="form-control form-control-md" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value }) } />
-									</div>
+										<h6 class="mb-4 text-center">Already have an account ? <a href="/login">Login</a></h6>
 
-									<div class="form-outline mb-3">
-										<label class="form-label" for="username">Username</label>
-										<input type="text" id="username" class="form-control form-control-md" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value }) } />
-									</div>
-
-									<div class="form-outline mb-3">
-										<label class="form-label" for="password">Password</label>
-										<input type="password" id="password" class="form-control form-control-md" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value }) } />
-									</div>
-
-									<div class="d-grid mx-auto" style={{height: '32px', }}>
-										<div class="err text-center" style={{fontSize: '10px', color: 'red', lineHeight: '32px', }}>
-										{ this.state.err }
+										<div class="form-outline mb-3">
+											<label class="form-label" for="name">Name</label>
+											<input type="text" id="name" class="form-control form-control-md" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value }) } />
 										</div>
-									</div>
 
-									<div class="d-grid gap-2 mx-auto">
-										<button class="btn btn-primary btn-md" type="submit" onClick={this.submit.bind(this)}>Create Account</button>
-									</div>
-									
+										<div class="form-outline mb-3">
+											<label class="form-label" for="username">Username</label>
+											<input type="text" id="username" class="form-control form-control-md" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value }) } style={{ textTransform: 'lowercase' }} />
+										</div>
+
+										<div class="form-outline mb-3">
+											<label class="form-label" for="password">Password</label>
+											<input type="password" id="password" class="form-control form-control-md" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value }) } />
+										</div>
+
+										<div class="d-grid mx-auto" style={{height: '32px', }}>
+											<div class="err text-center" style={{fontSize: '10px', color: 'red', lineHeight: '32px', }}>
+											{ this.state.err }
+											</div>
+										</div>
+
+										<div class="d-grid gap-2 mx-auto">
+											<button class="btn btn-primary btn-md" type="submit" onClick={this.submit.bind(this)}>Create Account</button>
+										</div>
+									</form>
 
 								</div>
 							</div>
