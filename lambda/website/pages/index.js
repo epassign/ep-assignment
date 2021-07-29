818780257e3e76c3c551787576caa5ff09eb6200
Adrian Praja <adrian@databank.ro>
2021-07-29 15:57:19 +0300

    pusher from cdn

--- lambda/website/pages/index.js
+++ lambda/website/pages/index.js
@@ -7,9 +7,10 @@ module.exports = function( event, cb ) {
 		<title>Index</title>
 		<meta charset="UTF-8">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
 		<script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
 		<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+		<script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+
 		<script src="https://${process.env.cdn_master}/ep-assignment.min.js"></script>
 
 	</head>
