17227f568f9423720827ced338afffd9d04d0526
Adrian Praja <adrian@databank.ro>
2021-07-29 17:23:04 +0300

    refresh leaderboard when notified by pusher

--- client/src/leaderboard.js
+++ client/src/leaderboard.js
@@ -1,21 +1,33 @@
 
 import { api_get } from './common';
 
+// this is hardcoded here and in btc_history on insert handler
+const global_realtime_channel = '367544a50c6f7d9b454fbc14114833d7'
+
 class Leaderboard extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			top: []
 		}
+		this.refresh = this.refresh.bind(this)
 	}
 
-	componentDidMount() {
+	refresh() {
 		api_get('/v1/top', ( err, data ) => {
 			if (err)
 				return;
 
 			this.setState({ top: data.top }) 
 		})
+	}
+	componentDidMount() {
+		this.refresh()
+
+		var global_channel = pusher.subscribe( global_realtime_channel );
+		global_channel.bind('top', (data) => {
+			this.refresh()
+		});
 	}
 	render() {
 		return (
